5ed727131922889d807d117415869adb46f02538
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2023-03-11 18:31:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TELEGRAMBOTTOKEN = os.environ.get('API_BOT')
-# TELEGRAMBOTTOKEN = "5804772518:AAG2yNDZbLzSgCFdiHqwCXFkdyLNhsoux_g"
+# TELEGRAMBOTTOKEN = os.environ.get('API_BOT')
+TELEGRAMBOTTOKEN = "5804772518:AAG2yNDZbLzSgCFdiHqwCXFkdyLNhsoux_g"
 OPENAITOKEN = os.environ.get('API_AI')
 # OPENAITOKEN = "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"
 MYUSERNAME = os.environ.get('BOT_TAG')
