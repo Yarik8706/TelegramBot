5040f4f4bdfe0852acdb49c0a7922a919ad9f324
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2023-03-11 18:16:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 TELEGRAMBOTTOKEN = os.environ.get('API_BOT')#"5804772518:AAG2yNDZbLzSgCFdiHqwCXFkdyLNhsoux_g"
 OPENAITOKEN = os.environ.get('API_AI')
 MYUSERNAME = os.environ.get('BOT_TAG')#"exweather_bot"
+ 
